fe3d540eaf0b2a81dc9c99285005df601747a6ce
Ar3T <aretx100@gmail.com>
2021-04-27 15:49:08 +0200

    Arreglando version 2.5

--- src/main/HolaMundo.java
+++ src/main/HolaMundo.java
@@ -1,24 +1,45 @@
 package main;
 
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HolaMundo
+ */
 public class HolaMundo extends HttpServlet {
- private static final long serialVersionUID = 1L;
- public void doGet(HttpServletRequest request, HttpServletResponse response)
- throws IOException, ServletException
- {
- String preTituloHTML5 = "<!DOCTYPE html>\n<html>\n<head>\n"
- + "<meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" />";
- response.setContentType("text/html");
- PrintWriter out = response.getWriter();
- out.println(preTituloHTML5);
- out.println("<title>Hola Mundo</title>");
- out.println("</head>");
- out.println("<body>");
- out.println("<h1>Hola Mundo!</h1>");
- out.println("</body>");
- out.println("</html>");
- //cambio hola
- }
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HolaMundo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+   		 throws IOException, ServletException
+   		 {
+   		 String preTituloHTML5 = "<!DOCTYPE html>\n<html>\n<head>\n"
+   		 + "<meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" />";
+   		 response.setContentType("text/html");
+   		 PrintWriter out = response.getWriter();
+   		 out.println(preTituloHTML5);
+   		 out.println("<title>Hola Mundo</title>");
+   		 out.println("</head>");
+   		 out.println("<body>");
+   		 out.println("<h1>Hola Mundo!</h1>");
+   		 out.println("</body>");
+   		 out.println("</html>");
+   		 //cambio hola
+   		 }
+
 }
