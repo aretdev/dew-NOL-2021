ffa1c6ea8504c029df74fe0e3e0cb31262d82474
Ar3T <aretx100@gmail.com>
2021-04-29 13:33:41 +0200

    logegr0

--- src/main/logger0.java
+++ src/main/logger0.java
@@ -1,6 +1,12 @@
 package main;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +19,9 @@ public class logger0 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
+     * 
+     * File file1 = new File(this.getServletContext().getRealPath("/") + "/WEB-INF/logs/log.log");
+     * 
      * @see HttpServlet#HttpServlet()
      */
     public logger0() {
@@ -24,15 +33,20 @@ public class logger0 extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter pw = response.getWriter();
+		String preTituloHTML5 = "<!DOCTYPE html>\n<html>\n<head>\n"
+		 + "<meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" />";
+		 response.setContentType("text/html");
+		pw.println(preTituloHTML5);
+		pw.println(LocalDateTime.now().toString() + " " + request.getRequestURI() + " " + request.getRemoteAddr() + " " + request.getServerName() + " " + request.getMethod() +" \n");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		Date date = new Date(System.currentTimeMillis());
 		doGet(request, response);
 	}
 
