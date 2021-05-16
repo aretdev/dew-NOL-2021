45a82a0ce514620aa57d5c3d05fbc5ff9380fa00
user <user@DEW-masanru6-2021.dsic.cloud>
2021-05-16 11:29:32 +0200

    commit mario

---
+++ src/main/alumnoApi.java
@@ -0,0 +1,91 @@
+package main;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.cookie.BasicCookieStore;
+import org.apache.hc.client5.http.cookie.Cookie;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpHeaders;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.json.JSONArray;
+
+/**
+ * Servlet implementation class alumnoApi
+ */
+public class alumnoApi extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public alumnoApi() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession ses = request.getSession(false);
+        String key = ses.getAttribute("key").toString();
+    	BasicCookieStore cookieStore = new BasicCookieStore();
+    	CloseableHttpClient httpclient = HttpClients.custom()
+        	    .setDefaultCookieStore(cookieStore)
+        	    .build();
+    	
+    	List<Cookie> cookies = (List<Cookie>) ses.getAttribute("cookie");
+    	String dni = ses.getAttribute("dni").toString();
+		
+    	if(request.isUserInRole("rolalu")) {
+    		
+    		HttpGet httpGet = new HttpGet("http://dew-masanru6-2021.dsic.cloud:9090/CentroEducativo/alumnos/"+dni+"/asignaturas?key="+key);
+            httpGet.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+            cookieStore.addCookie(cookies.get(0));
+            
+            CloseableHttpResponse response1 = httpclient.execute(httpGet);	
+            String content = "-1";
+            HttpEntity entity1 = response1.getEntity();
+            
+            try {
+            	content = EntityUtils.toString(entity1);
+            }catch (ParseException e) {System.out.println("Error entity");}
+            
+            EntityUtils.consume(entity1);
+            response1.close();
+            
+            JSONArray o = new JSONArray(content);
+    	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
